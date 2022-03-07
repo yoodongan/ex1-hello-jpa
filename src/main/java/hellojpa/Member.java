55333583a8bb144c24fbbceb2c468206ee66b172
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-03-07 09:24:06 +0900

    Initial commit

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,37 @@
+package hellojpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    private Long id;
+
+    @Column(unique = true, length = 10)
+    private String name;
+
+    private int age;
+
+    public Member(){}
+
+    public Member(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
