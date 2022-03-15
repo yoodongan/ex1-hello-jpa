7cd77547b4eff4835c16e168da26231813d10b88
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-03-15 11:42:24 +0900

    update

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,20 +15,37 @@ public class JpaMain {
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
-        //코드
-        try{
+
+        try {
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
             Member member = new Member();
-
-            member.setUsername("C");
-
+            member.setUsername("member1");
+            member.setTeam(team);
             em.persist(member);
 
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+            List<Member> results = findMember.getTeam().getMembers();
+            for (Member result : results) {
+                System.out.println("리스트 멤버 목록 : " + result.getUsername());
+            }
+
             tx.commit();
-        }catch(Exception e){
+        } catch(Exception e){
             tx.rollback();
-        }finally{
+        } finally {
             em.close();
         }
+
+
         emf.close();
+
+
     }
+
 }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,19 +3,18 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
-@SequenceGenerator(
-        name = "member_seq_generator",
-        sequenceName = "member_seq", // 매핑할 데이터 시퀀스 이름
-        initialValue = 1, allocationSize = 50)
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "USERNAME")
     private String username;
 
-    public Member(){}
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
     public Long getId() {
         return id;
@@ -31,5 +30,13 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
